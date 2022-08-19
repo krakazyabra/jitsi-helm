824fdebd22c37bea89dd6f6dae3ec4f72371f692
Ben Banfield-Zanin <benbz@matrix.org>
2022-08-19 13:41:43 +0100

    JVB: support running with hostPort rather than nodePort

--- configs/jvb/entrypoint.sh
+++ configs/jvb/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-
+{{ if empty $.Values.jvb.hostPort }}
 # JVB baseport can be passed to this script
 if [[ "$1" =~ ^[0-9]+$ ]]; then
     BASE_PORT=$1
@@ -11,6 +11,9 @@ fi
 
 # add jvb ID to the base port (e.g. 30300 + 1 = 30301)
 export JVB_PORT=$(($BASE_PORT+${HOSTNAME##*-}))
+{{- else }}
+export JVB_PORT="{{ $.Values.jvb.hostPort }}"
+{{- end }}
 echo "JVB_PORT=$JVB_PORT"
 
 exec "$@"
